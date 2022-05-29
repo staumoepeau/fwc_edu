f5ba0e6ab6b97024beb95163b7a01b3875e5452d
staumoepeau <sione.taumoepeau@gmail.com>
2022-05-30 09:00:41 +1300

    Update Education

---
+++ fwc_edu/fwc_education/doctype/campus/campus.js
@@ -0,0 +1,8 @@
+// Copyright (c) 2022, Sione Taumoepeau and contributors
+// For license information, please see license.txt
+
+frappe.ui.form.on('Campus', {
+	// refresh: function(frm) {
+
+	// }
+});
---
+++ fwc_edu/fwc_education/doctype/campus/test_campus.py
@@ -0,0 +1,8 @@
+# Copyright (c) 2022, Sione Taumoepeau and Contributors
+# See license.txt
+
+# import frappe
+import unittest
+
+class TestCampus(unittest.TestCase):
+	pass
