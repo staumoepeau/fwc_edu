45ee5244611cdb09aa27553525d3f607fadd017f
staumoepeau <sione.taumoepeau@gmail.com>
2022-11-23 05:35:17 +1300

    Update Reports QSC

---
+++ fwc_edu/fwc_education/report/qsc_subject_group_report/qsc_subject_group_report.js
@@ -0,0 +1,9 @@
+// Copyright (c) 2022, Sione Taumoepeau and contributors
+// For license information, please see license.txt
+/* eslint-disable */
+
+frappe.query_reports["QSC Subject Group Report"] = {
+	"filters": [
+
+	]
+};
